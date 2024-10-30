1730eef7eba99f21684754aa6e4e3539af99f14c
Roberto M. Leitão <93278319+betoleitao5@users.noreply.github.com>
2024-10-30 10:05:20 -0300

    script (falha)
    cadeeeeeeeeee meu sripitiiiiiiiiii

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ document.getElementById('closeButton').addEventListener('click', function() {
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'none'; // Oculta a sobreposição
     document.getElementById('map').style.zIndex = '1';    });
+
+    console.log("hellou")
