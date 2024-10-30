b22f49b1131da2b99738db90e841e1f34cf515bd
Roberto M. Leitão <93278319+betoleitao5@users.noreply.github.com>
2024-10-30 09:39:13 -0300

    scripts
    corrigindo scriptis

--- js/map.js
+++ js/map.js
@@ -58,9 +58,5 @@
         });
     });
 
-document.getElementById('closeButton').addEventListener('click', function() {
-    const overlay = document.getElementById('overlay');
-    overlay.style.display = 'none'; // Oculta a sobreposição
-    document.getElementById('map').style.zIndex = '1';    });
 
    
