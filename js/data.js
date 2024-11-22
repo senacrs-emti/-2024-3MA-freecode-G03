f2d17aead1b9bf69cb82c834594ac861e9ebf5fe
Nathalia Alencastro <nathalia.alencastro00@gmail.com>
2024-11-22 08:10:58 -0300

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,8 @@
+const markers = [
+    { position: { lat: -30.025296, lng: -51.211922 }, title: "Senac Distrito Criativo", icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png" },
+    { position: { lat: 48.855451, lng: 2.350692 }, title: "França - Paris", icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" },
+    { position: { lat: 26.404693, lng: 29.762399 }, title: "Egito", icon: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png" },
+    { position: { lat: 39.409713, lng: 22.508420 }, title: "Grécia", icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" },
+    { position: { lat: 41.895742, lng: 12.481967 }, title: "Roma", icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" },
+    { position: { lat: 39.919946, lng: 116.413294 }, title: "China", icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" }
+];
